e3b9410c0cb674b0e2a9dadb8094c4e3675954ac
yxm <yinxiaoming96@gmail.com>
2017-11-05 20:35:13 +0800

    添加 mybatis 环境

--- src/main/java/betahouse/ThemisApplication.java
+++ src/main/java/betahouse/ThemisApplication.java
@@ -1,5 +1,6 @@
 package betahouse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ServletComponentScan
 @ComponentScan
+@MapperScan("betahouse.model.mapper")
 @SpringBootApplication
 public class ThemisApplication {
 
